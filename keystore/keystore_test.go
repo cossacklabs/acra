1a638fac08a99fe4992f0c4b20c178bf0ee51038
andrey <andrey@cossacklabs.com>
2016-12-01 20:32:40 +0200

    Add key folder permission check

--- keystore/keystore_test.go
+++ keystore/keystore_test.go
@@ -28,6 +28,9 @@ func TestFileSystemKeyKeyStore(t *testing.T) {
 		os.RemoveAll(key_directory)
 	}()
 	store, err := keystore.NewFilesystemKeyStore(key_directory)
+	if err != nil{
+		t.Fatal("error")
+	}
 	if store.HasZonePrivateKey([]byte("non-existent key")) {
 		t.Fatal("Expected false on non-existent key")
 	}
