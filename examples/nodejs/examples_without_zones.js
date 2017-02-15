51cac683cf253cfe07ecbfe65c8d0004d4975bdf
andrey <andrey@cossacklabs.com>
2017-02-15 16:40:26 +0200

    Add PHP and nodejs examples for zoneless acra

--- examples/nodejs/examples_without_zones.js
+++ examples/nodejs/examples_without_zones.js
@@ -1,10 +1,5 @@
 var acra = require('acrawriter');
 var pg = require('pg');
-var request = require('sync-request');
-
-var res = request('GET', 'http://127.0.0.1:9191/getNewZone');
-var zone = JSON.parse(res.getBody().toString('utf8'));
-console.log(zone);
 
 var raw_data = 'Test Message';
 
@@ -23,20 +18,20 @@ pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('CREATE TABLE IF NOT EXISTS testjs_zone(id SERIAL PRIMARY KEY, zone BYTEA, data BYTEA, raw_data TEXT)', [], function(err, res) {
+  client.query('CREATE TABLE IF NOT EXISTS testjs(id SERIAL PRIMARY KEY, data BYTEA, raw_data TEXT)', [], function(err, res) {
     done();
     if(err) {
       return console.error('error running query', err);
     }
   });
-  client.query('insert into testjs_zone (zone, data, raw_data) values ($1, $2, $3)', [zone.id, acra.create_acra_struct(raw_data, new Buffer(zone.public_key, 'base64'), zone.id), raw_data], function(err, res) {
+  client.query('insert into testjs (data, raw_data) values ($1, $2)', [acra.create_acra_struct(raw_data, new Buffer(zone.public_key, 'base64'), ""), raw_data], function(err, res) {
     done();
     if(err) {
       return console.error('error running query', err);
     }
   });
 
-  client.query('select zone, data, raw_data from testjs_zone', [], function(err, res) {
+  client.query('select data, raw_data from testjs', [], function(err, res) {
     done();
     if(err) {
       return console.error('error running query', err);
