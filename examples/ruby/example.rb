58b478e3c75034ba844b627145a60d34ad4ccd07
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2017-06-08 14:32:38 +0300

    update ruby examples

--- examples/ruby/example.rb
+++ examples/ruby/example.rb
@@ -12,16 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 require 'pg'
-require 'acra'
+require 'acrawriter'
 
 
 conn = PG.connect( dbname: 'acra', host: '127.0.0.1', port: '9494', user: 'postgres', password: 'postgres' )
 conn.exec('CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, data BYTEA, raw_data TEXT);')
 
 
-acra_public = File.read(File.expand_path(".acrakeys/client_server.pub"))
+acra_public = File.read(File.expand_path(".acrakeys/client_storage.pub"))
 some_data = (0...8).map { (65 + rand(26)).chr }.join
-acrastruct = create_acra_struct(some_data, acra_public)
+acrastruct = create_acrastruct(some_data, acra_public)
 rand_id = rand(100000)
 conn.exec_params("INSERT INTO test(id, data, raw_data) VALUES ($1, $2, $3);", [rand_id, conn.escape_bytea(acrastruct), some_data])
 
--- examples/ruby/example_with_zone.rb
+++ examples/ruby/example_with_zone.rb
@@ -14,10 +14,10 @@
 require 'json'
 require "base64"
 require 'pg'
-require 'acra'
+require 'acrawriter'
 
 # output from ./acra_addzone. you should assign your values
-zone_data = JSON.parse('{"id":"ZXCfelcOOnfYWkVphBn","public_key":"VUVDMgAAAC1YtTViA+VtaV0qh/OB7Rr3h0U04iQ0pTfPTh9gur62nerv4fsx"}')
+zone_data = JSON.parse('{"id":"DDDDDDDDhydxgNoxWYxYRZzf","public_key":"VUVDMgAAAC11q5/TAmIAS42yyuNISRCsbl56D/yBH0iSZ9nmVfhdaOP0mwSH"}')
 
 conn = PG.connect( dbname: 'acra', host: '127.0.0.1', port: '9494', user: 'postgres', password: 'postgres' )
 conn.exec('CREATE TABLE IF NOT EXISTS test2(id INTEGER PRIMARY KEY, zone bytea, data BYTEA, raw_data TEXT);')
@@ -25,7 +25,7 @@ conn.exec('CREATE TABLE IF NOT EXISTS test2(id INTEGER PRIMARY KEY, zone bytea, 
 zone_public = Base64.decode64(zone_data["public_key"])
 zone_id = zone_data["id"]
 some_data = (0...8).map { (65 + rand(26)).chr }.join
-acrastruct = create_acra_struct(some_data, zone_public, zone_id)
+acrastruct = create_acrastruct(some_data, zone_public, zone_id)
 
 rand_id = rand(100000)
 conn.exec_params("INSERT INTO test2(id, zone, data, raw_data) VALUES ($1, $2, $3, $4);", [rand_id, conn.escape_bytea(zone_id), conn.escape_bytea(acrastruct), some_data])
