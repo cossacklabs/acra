83f51e28d24584d6755ec25dd80b791ff5baa50e
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2018-03-21 14:04:25 +0200

    use correct ssl mode for "close connections" test

--- tests/test.py
+++ tests/test.py
@@ -95,10 +95,10 @@ def stop_process(process):
             traceback.print_exc()
 
 
-def get_connect_args(port=5432, sslmode='require', **kwargs):
+def get_connect_args(port=5432, sslmode=None, **kwargs):
     args = connect_args.copy()
     args['port'] = port
-    args['sslmode'] = sslmode
+    args['sslmode'] = sslmode if sslmode else SSLMODE
     args.update(kwargs)
     return args
 
