c3330c99044518bdd42d7316df35ad2fdaae61b0
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2018-05-31 14:58:38 +0300

    generate config for authmanager

--- configs/regenerate.sh
+++ configs/regenerate.sh
@@ -6,3 +6,4 @@ go run ./cmd/acra-webconfig/*.go --dump_config
 go run ./cmd/acra-rollback/*.go --dump_config
 go run ./cmd/acra-keymaker/*.go --dump_config
 go run ./cmd/acra-poisonrecordmaker/*.go --dump_config
+go run ./cmd/acra-authmanager/*.go --dump_config
