ddd54cd09edadf0c89e1e0df48177ccd1b546475
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2018-06-01 13:07:20 +0300

    support mysql in examples for golang

--- examples/golang/src/example/example.go
+++ examples/golang/src/example/example.go
@@ -17,75 +17,103 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
+	"math/rand"
+
 	"github.com/cossacklabs/acra/acra-writer"
-	"github.com/cossacklabs/acra/keystore"
 	"github.com/cossacklabs/acra/utils"
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
-	"math/rand"
-	"time"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const CONNECTION_STRING string = "user=andrey dbname=acra host=127.0.0.1 port=9494"
-
-func RandString(n int) []byte {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return b
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	acraPublic, err := utils.LoadPublicKey(fmt.Sprintf("%v/client_server.pub", keystore.DEFAULT_KEY_DIR_SHORT))
+	mysql := flag.Bool("mysql", false, "Use MySQL driver")
+	_ = flag.Bool("postgresql", false, "Use PostgreSQL driver (default if nothing else set)")
+	dbname := flag.String("dbname", "acra", "Database name")
+	host := flag.String("host", "127.0.0.1", "Database host")
+	port := flag.Int("port", 9494, "Database port")
+	user := flag.String("user", "test", "Database user")
+	password := flag.String("password", "password", "Database user's password")
+	data := flag.String("data", "", "Data to save")
+	printData := flag.Bool("print", false, "Print data from database")
+	publicKey := flag.String("public_key", "", "Path to public key")
+	flag.Parse()
+
+	connectionString := fmt.Sprintf("user=%v password=%v dbname=%v host=%v port=%v", *user, *password, *dbname, *host, *port)
+	driver := "postgres"
+	if *mysql {
+		// username:password@protocol(address)/dbname?param=value
+		// https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		connectionString = fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", *user, *password, *host, *port, *dbname)
+		driver = "mysql"
+	}
+
+	acraPublic, err := utils.LoadPublicKey(*publicKey)
 	if err != nil {
 		panic(err)
 	}
-	//some_data := []byte("test data for acra from go")
-	someData := RandString(20)
-	fmt.Printf("Generated test data: %v\n", string(someData))
 
-	acrastruct, err := acrawriter.CreateAcrastruct(someData, acraPublic, nil)
-	if err != nil {
-		log.Fatal("can't create acrastruct - ", err)
-	}
-
-	db, err := sql.Open("postgres", CONNECTION_STRING)
+	db, err := sql.Open(driver, connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Create test table with command: 'CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, data BYTEA, raw_data TEXT);'")
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, data BYTEA, raw_data TEXT);")
-	if err != nil {
-		panic(err)
-	}
 
-	fmt.Println("Insert test data to table")
-	_, err = db.Exec("insert into test (id, data, raw_data) values ($1, $2, $3);", rand.Int31(), acrastruct, string(someData))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Select from db with command: 'SELECT data, raw_data FROM test;'")
-	rows, err := db.Query(`SELECT data, raw_data FROM test;`)
-	defer rows.Close()
+	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var data []byte
-	var rawData string
-	fmt.Println("data - raw_data")
-	for rows.Next() {
-		err := rows.Scan(&data, &rawData)
+
+	if *mysql {
+		query := "CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, data VARBINARY(1000), raw_data VARCHAR(1000));"
+		fmt.Printf("Create test table with command: '%v'\n", query)
+		_, err = db.Exec(query)
+
+	} else {
+		query := "CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, data BYTEA, raw_data TEXT);"
+		fmt.Printf("Create test table with command: '%v'\n", query)
+		_, err = db.Exec(query)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	if *data != "" {
+		acrastruct, err := acrawriter.CreateAcrastruct([]byte(*data), acraPublic, nil)
 		if err != nil {
-			fmt.Println("ERROR")
-			fmt.Println(err)
-			return
+			log.Fatal("can't create acrastruct - ", err)
 		}
-		fmt.Printf("data: %v\nraw_data: %v\n\n", string(data), string(rawData))
+		fmt.Println("Insert test data to table")
+		if *mysql {
+			_, err = db.Exec("insert into test (id, data, raw_data) values (?, ?, ?);", rand.Int31(), acrastruct, *data)
+		} else {
+			_, err = db.Exec("insert into test (id, data, raw_data) values ($1, $2, $3);", rand.Int31(), acrastruct, *data)
+		}
+		if err != nil {
+			panic(err)
+		}
+	} else if *printData {
+		query := `SELECT data, raw_data FROM test;`
+		fmt.Printf("Select from db with command: '%v'\n", query)
+		rows, err := db.Query(query)
+		defer rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var data []byte
+		var rawData string
+		fmt.Println("data - raw_data")
+		for rows.Next() {
+			err := rows.Scan(&data, &rawData)
+			if err != nil {
+				fmt.Println("ERROR")
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf("data: %v\nraw_data: %v\n\n", string(data), string(rawData))
+		}
 	}
+
 	fmt.Println("Finish")
 }
