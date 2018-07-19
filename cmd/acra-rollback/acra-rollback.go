ae672faf8b2beaa385e7f3c0ada25da578799f57
Lagovas <lagovas.lagovas@gmail.com>
2018-07-20 02:15:30 +0300

    check that insert query contain placeholder (#210)

--- cmd/acra-rollback/acra-rollback.go
+++ cmd/acra-rollback/acra-rollback.go
@@ -183,6 +183,11 @@ func main() {
 		PLACEHOLDER = "?"
 	}
 
+	if !strings.Contains(*sqlInsert, PLACEHOLDER) {
+		log.Errorln("SQL INSERT statement doesn't contain any placeholders")
+		os.Exit(1)
+	}
+
 	dbDriverName := "postgres"
 	if *useMysql {
 		// https://github.com/ziutek/mymysql
