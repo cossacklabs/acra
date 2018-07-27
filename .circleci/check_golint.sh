93e9bd5b0fc33026fc6623a51234c331ba4d2652
Vixentael <vixentael@gmail.com>
2018-07-27 03:30:49 +0300

    install golint via apt-get

--- .circleci/check_golint.sh
+++ .circleci/check_golint.sh
@@ -3,12 +3,12 @@
 # run golint and count output lines
 # golint print issues it found in source files
 # count lines with wc and check that 0 lines was in output
-result=$($GOPATH/bin/golint ./... | wc -l)
+result=$(golint ./... | wc -l)
 if [[ $result -gt 400 ]]; then
   # too many golint issues
   echo "Too many golint issues: $result"
   exit 1;
 else
-  echo "Don't have many golint issues: $result"
+  echo "OK: don't have many golint issues: $result"
   exit 0;
 fi;
