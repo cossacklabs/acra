f46b6a44f7b69f8ad292714e25d32c1c4e0f8678
Vixentael <vixentael@gmail.com>
2018-07-27 03:05:30 +0300

    play with golint auto check

---
+++ .circleci/check_golint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# run golint and count output lines
+# golint print issues it found in source files
+# count lines with wc and check that 0 lines was in output
+result=$($GOPATH/bin/golint ./... | wc -l)
+if [[ $result -gt 400 ]]; then
+  # too many golint issues
+  echo "Too many golint issues: $result"
+  exit 1;
+else
+  echo "Don't have many golint issues: $result"
+  exit 0;
+fi;
