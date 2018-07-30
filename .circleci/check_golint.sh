6192100df7a12d7a1c5e663ab7b941979cf68abf
Vixentael <vixentael@gmail.com>
2018-07-30 16:48:55 +0300

    setup golint again

--- .circleci/check_golint.sh
+++ .circleci/check_golint.sh
@@ -3,7 +3,7 @@
 # run golint and count output lines
 # golint print issues it found in source files
 # count lines with wc and check that 0 lines was in output
-result=$(golint ./... | wc -l)
+result=$($GOPATH/bin/golint ./... | wc -l)
 if [[ $result -gt 450 ]]; then
   # too many golint issues
   echo "Too many golint issues: $result"
