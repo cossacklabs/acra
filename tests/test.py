f772f4973dfa43a4c55d068f81af08c72233def2
Vixentael <vixentael@gmail.com>
2018-08-11 11:13:38 +0300

    try to decode log

--- tests/test.py
+++ tests/test.py
@@ -1523,8 +1523,8 @@ class AcraCatchLogsMixin(object):
     def read_log(self, process):
         with open(self.log_files[process].name, 'r', errors='replace',
                   encoding='utf-8') as f:
-            log = f.read()
-            #print(log)
+            log = f.read().decode(encoding='utf-8', errors='replace')
+            print(log)
             return log
 
     def fork_acra(self, popen_kwargs: dict=None, **acra_kwargs: dict):
