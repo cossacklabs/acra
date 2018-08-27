7aade494b27749b31e5ca7c88f043a6e8db271d6
vixentael <vixentael@users.noreply.github.com>
2018-08-27 11:42:51 +0300

    AcraWriter for Java/Android (#252)
    * PoC of acrawriter android
    
    * refactor-rename packages, move acrawriter into separate package
    
    * folders re-org and tests
    
    * move library to /lib
    
    * move example to examples folder, separate from lib in wrappers folder
    
    * add readme, add comments
    
    * add comments, calculate buffer length, flush buffer
    
    * updated tests to generate large random values as acrastruct payload
    
    * decrease sdk version to 16

---
+++ examples/android_java/AcraWriterAndroidApp/app/src/androidTest/java/com/cossacklabs/acrawriter/test/AcraWriterTest.java
@@ -0,0 +1,183 @@
+package com.cossacklabs.acrawriter.test;
+
+import com.cossacklabs.acrawriter.*;
+import com.cossacklabs.themis.AsymmetricKey;
+import com.cossacklabs.themis.InvalidArgumentException;
+import com.cossacklabs.themis.KeyGenerationException;
+import com.cossacklabs.themis.Keypair;
+import com.cossacklabs.themis.KeypairGenerator;
+import com.cossacklabs.themis.NullArgumentException;
+import com.cossacklabs.themis.PublicKey;
+import com.cossacklabs.themis.SecureCell;
+import com.cossacklabs.themis.SecureCellData;
+import com.cossacklabs.themis.SecureCellException;
+import com.cossacklabs.themis.SecureMessage;
+import com.cossacklabs.themis.SecureMessageWrapException;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.Random;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+public class AcraWriterTest {
+
+    @Test
+    public void testCreateEmptyAcraStruct() {
+        AcraWriter aw = new AcraWriter();
+        assertNotNull(aw);
+
+        try {
+            AcraStruct a = aw.createAcraStruct(null, null, null);
+            AcraStruct b = aw.createAcraStruct("message".getBytes(), null, null);
+        } catch (NullArgumentException e) {
+            // completely fine
+        } catch (KeyGenerationException | InvalidArgumentException | SecureCellException | IOException e) {
+            assertTrue("If no message or pubkey, NullArgumentException should be generated", false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateAcraStructWithInvalidKey() {
+        AcraWriter aw = new AcraWriter();
+        assertNotNull(aw);
+
+        try {
+            AcraStruct as = aw.createAcraStruct("message".getBytes(), new PublicKey("some pub key".getBytes()), null);
+        } catch (InvalidArgumentException e) {
+            // completely fine
+        } catch (KeyGenerationException | NullArgumentException | SecureCellException | IOException e) {
+            assertTrue("If pub key is invalid, InvalidArgumentException should be generated", false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateAcraStructWithoutZone() {
+        AcraWriter aw = new AcraWriter();
+        assertNotNull(aw);
+
+        try {
+            Keypair keypair = KeypairGenerator.generateKeypair(AsymmetricKey.KEYTYPE_EC);
+            AcraStruct as = aw.createAcraStruct("message".getBytes(), keypair.getPublicKey(), null);
+        } catch (InvalidArgumentException | KeyGenerationException | NullArgumentException | SecureCellException | IOException e) {
+            assertTrue("Shouldn't be any exceptions", false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateAcraStructWithZone() {
+        AcraWriter aw = new AcraWriter();
+        assertNotNull(aw);
+
+        try {
+            Keypair keypair = KeypairGenerator.generateKeypair(AsymmetricKey.KEYTYPE_EC);
+            AcraStruct as = aw.createAcraStruct("message".getBytes(), keypair.getPublicKey(), "zone ID".getBytes());
+        } catch (InvalidArgumentException | KeyGenerationException | NullArgumentException | SecureCellException | IOException e) {
+            assertTrue("Shouldn't be any exceptions", false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateAcraStructFormat() {
+        createAndTestAcraStructWithPayloadSize(10); // 10 bytes
+        createAndTestAcraStructWithPayloadSize(100); // 100 bytes
+        createAndTestAcraStructWithPayloadSize(1024); // 1 Kb
+        createAndTestAcraStructWithPayloadSize(100 * 1024); // 100 Kb
+        createAndTestAcraStructWithPayloadSize(1024 * 1024); // 1 Mb
+        createAndTestAcraStructWithPayloadSize(10 * 1024 * 1024); // 10 Mb
+//        createAndTestAcraStructWithPayloadSize(100 * 1024 * 1024); // 100 Mb
+//        createAndTestAcraStructWithPayloadSize(1024 * 1024 * 1024); // 1 Gb
+//        createAndTestAcraStructWithPayloadSize(2 * 1024 * 1024 * 1024); // 2 Gb
+//        createAndTestAcraStructWithPayloadSize(4 * 1024 * 1024 * 1024); // 4 Gb
+    }
+
+    private void createAndTestAcraStructWithPayloadSize(int payloadSize) {
+        AcraWriter aw = new AcraWriter();
+        assertNotNull(aw);
+
+        try {
+            byte[] message = new byte[payloadSize];
+            new Random().nextBytes(message);
+
+            byte[] zoneID = "some zone id".getBytes();
+            Keypair keypair = KeypairGenerator.generateKeypair(AsymmetricKey.KEYTYPE_EC);
+
+            AcraStruct acraStruct = aw.createAcraStruct(message, keypair.getPublicKey(), zoneID);
+
+            byte[] acraStructBytes = acraStruct.toByteArray();
+            ByteArrayInputStream stream = new ByteArrayInputStream(acraStructBytes);
+
+            // read header=
+            int headerLength = 8;
+            byte[] header = new byte[headerLength];
+            int i = stream.read(header, 0, headerLength);
+
+            for (byte b : header) {
+                assertTrue("Header byte should be 34", b == 34);
+            }
+
+            // pub key
+            int pubkeyLength = 45;
+            byte[] pubKeyBytes = new byte[pubkeyLength];
+            i = stream.read(pubKeyBytes, 0, pubkeyLength);
+            PublicKey pubKey = new PublicKey(pubKeyBytes);
+            assertNotNull("Should be able to read public key", pubKey);
+
+            // encrypted symm key
+            int encryptedSymKeyLength = 84;
+            byte[] encryptedSymKey = new byte[encryptedSymKeyLength];
+            i = stream.read(encryptedSymKey, 0, encryptedSymKeyLength);
+
+            // create decryptor
+            SecureMessage sm = new SecureMessage(keypair.getPrivateKey(), pubKey);
+            assertNotNull("Should be able to init SecureMessage from read public key", sm);
+
+            byte[] decryptedSymmKey = sm.unwrap(encryptedSymKey);
+            assertNotNull("Should be able to decrypt symm key", decryptedSymmKey);
+            assertTrue("Decrypted symm key should not be empty", decryptedSymmKey.length > 0);
+
+            // length of encrypted data
+            int encryptedDataLengthLength = 8;
+            byte[] encryptedDataLength = new byte[encryptedDataLengthLength];
+            i = stream.read(encryptedDataLength, 0, encryptedDataLengthLength);
+
+            // convert encrypted data length from little endian
+            ByteBuffer bb = ByteBuffer.wrap(encryptedDataLength);
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+
+            // technically, encryptedDataLength max size is uint64, which is 2^64-1,
+            // but in Java max array length is int, which is 2^32-1
+            int encryptedDataLengthI = bb.getInt();
+
+            // encrypted data
+            byte[] encryptedData = new byte[encryptedDataLengthI];
+            i = stream.read(encryptedData, 0, encryptedDataLengthI);
+            assertNotNull("Should be able to read encrypted data", encryptedData);
+            assertTrue("Encrypted data should not be empty", encryptedData.length > 0);
+
+            // create decryptor
+            SecureCell cell = new SecureCell(decryptedSymmKey);
+            byte[] decryptedData = cell.unprotect(zoneID, new SecureCellData(encryptedData, null));
+            assertNotNull("Should be able to decrypt encrypted data", decryptedData);
+            assertTrue("Decrypted data should not be empty", decryptedData.length > 0);
+
+            assertTrue("Encrypted and decrypted data should be equal", Arrays.equals(decryptedData, message));
+
+            stream.close();
+
+        } catch (InvalidArgumentException | KeyGenerationException | NullArgumentException | SecureCellException | IOException | SecureMessageWrapException e) {
+            e.printStackTrace();
+            assertTrue("Shouldn't be any exceptions", false);
+        }
+    }
+}
---
+++ wrappers/java/com/cossacklabs/acrawriter/AcraWriter.java
@@ -0,0 +1,116 @@
+package com.cossacklabs.acrawriter;
+
+import com.cossacklabs.themis.AsymmetricKey;
+import com.cossacklabs.themis.InvalidArgumentException;
+import com.cossacklabs.themis.KeyGenerationException;
+import com.cossacklabs.themis.Keypair;
+import com.cossacklabs.themis.KeypairGenerator;
+import com.cossacklabs.themis.NullArgumentException;
+import com.cossacklabs.themis.PrivateKey;
+import com.cossacklabs.themis.PublicKey;
+import com.cossacklabs.themis.SecureCell;
+import com.cossacklabs.themis.SecureCellData;
+import com.cossacklabs.themis.SecureCellException;
+import com.cossacklabs.themis.SecureMessage;
+import com.cossacklabs.themis.SecureMessageWrapException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+/**
+ * AcraWriter generates AcraStructs, specially encrypted data, from provided plaintext.
+ * AcraStruct encrypts data using mix of symmetric and asymmetric encryption,
+ * using Themis Secure Cell and Themis Secure Message. Data is encrypted with publicKey,
+ * that represents AcraStorage public key (known as <client_id>_storage.pub) or
+ * with AcraZonePublicKeys (known as <zone_id>_zone.pub).
+ *
+ * What is AcraWriter and how it works https://github.com/cossacklabs/acra/wiki/AcraConnector-and-AcraWriter
+ * What is Zone https://github.com/cossacklabs/acra/wiki/Zones
+ */
+public class AcraWriter {
+
+    private static byte kSymmetricKeySize = 32;
+
+    // AcraStruct header format has eight ' symbols, 34 is ascii code of '
+    private static byte kAcraStructHeaderByte = 34;
+
+    /**
+     * @discussion Method to generate AcraStruct from plain text message. Two option are possible: without Zones or with Zones.
+     * Without zones: `publicKey` is AcraStorage public key.
+     * With zones: `zoneID` is required, `publicKey` is Zone public key.
+     * @param message plaintext data to encrypt into AcraStruct.
+     * @param peerPublicKey either storage key or zone key, depending if client wants to use Zones.
+     * @param zoneID is optional, when client is using Zones. If zoneId is represented as string Id, expected input is [@"some zone id here" dataUsingEncoding:NSUTF8StringEncoding]
+     * @return data encrypted into AcraStruct format, or null on error
+     */
+    public AcraStruct createAcraStruct(byte[] message, PublicKey peerPublicKey, byte[] zoneID) throws NullArgumentException, KeyGenerationException, InvalidArgumentException, SecureCellException, IOException {
+        if (message == null || message.length == 0) {
+            throw new NullArgumentException("Message to encrypt is empty or not provided");
+        }
+
+        if (peerPublicKey == null || peerPublicKey.toByteArray().length == 0) {
+            throw new NullArgumentException("Peer public key is not provided");
+        }
+
+        // 1. generate EC keypair
+        Keypair throwAwayKeypair = KeypairGenerator.generateKeypair(AsymmetricKey.KEYTYPE_EC);
+
+        // 2. generate random symm key with @(kSymmetricKeySize) size
+        byte[] randomSymmetricKey = new byte[kSymmetricKeySize];
+        new SecureRandom().nextBytes(randomSymmetricKey);
+
+        // 3. encrypt random symmetric key using asymmetric encryption with random private key and acra/zone public key
+        PrivateKey privateKey = throwAwayKeypair.getPrivateKey();
+        SecureMessage sm = new SecureMessage(privateKey, peerPublicKey);
+        byte[] wrappedSymmetricKey;
+        try {
+            wrappedSymmetricKey = sm.wrap(randomSymmetricKey);
+        } catch (SecureMessageWrapException e) {
+            throw new InvalidArgumentException("Can't encrypt symmetric key: check if PublicKey is valid");
+        }
+
+        // zeroing private key
+        // how to zero private key if `toByteArray` returns clone?
+
+        // 4. encrypt payload using symmetric encryption and random symm key
+        SecureCell sc = new SecureCell(randomSymmetricKey, SecureCell.MODE_SEAL);
+        SecureCellData encryptedPayload = sc.protect(zoneID, message);
+        byte[] encryptedData = encryptedPayload.getProtectedData();
+
+        // convert encrypted data length to little endian
+
+        ByteBuffer bb = ByteBuffer.allocate(8); // 8 bytes, uint64
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.putLong(encryptedData.length);
+        byte[] encryptedDataLengthArray = bb.array();
+
+        // zeroing symm key
+        Arrays.fill(randomSymmetricKey, (byte)0);
+
+        // 5. pack acrastruct
+        byte[] throwAwayPublicKey = throwAwayKeypair.getPublicKey().toByteArray();
+        byte[] header = new byte[]{kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte, kAcraStructHeaderByte};
+        ByteArrayOutputStream output = new ByteArrayOutputStream(header.length + throwAwayPublicKey.length + wrappedSymmetricKey.length + encryptedDataLengthArray.length + encryptedData.length);
+
+        try {
+            output.write(header);
+            output.write(throwAwayPublicKey);
+            output.write(wrappedSymmetricKey);
+            output.write(encryptedDataLengthArray);
+            output.write(encryptedData);
+
+            AcraStruct as = new AcraStruct(output.toByteArray());
+            output.flush();
+
+            return as;
+
+        } catch (IOException e) {
+
+            throw new IOException("Can't wrap bytes into AcraStruct");
+        }
+    }
+}
