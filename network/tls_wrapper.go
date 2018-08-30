5bd80bb8643e694386075c2506e77393d6f87330
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2018-08-30 18:28:07 +0300

    change order of ciphers

--- network/tls_wrapper.go
+++ network/tls_wrapper.go
@@ -19,42 +19,22 @@ package network
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"io/ioutil"
-	"net"
-
 	"errors"
 	"github.com/cossacklabs/acra/logging"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net"
 	"time"
 )
 
 // allowedCipherSuits that set in default tls config
 var allowedCipherSuits = []uint16{
-	// took from https://golang.org/pkg/crypto/tls/#pkg-constants
-	//tls.TLS_RSA_WITH_RC4_128_SHA,
-	//tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-	//tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-	//tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	//tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
-	//tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-	//tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	//tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
-	//tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-	//tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-	//tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
-	//tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-	//tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-	//tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-	//tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-	//tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-	//tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-	//tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 }
 
 // TLSConnectionWrapper for wrapping connection into TLS encryption
@@ -137,7 +117,7 @@ func NewTLSConfig(serverName string, caPath, keyPath, crtPath string, authType t
 		Certificates: certificates,
 		ServerName:   serverName,
 		ClientAuth:   authType,
-		MinVersion:   tls.VersionTLS12,
+		MinVersion:   tls.VersionTLS11,
 		CipherSuites: allowedCipherSuits,
 	}, nil
 }
