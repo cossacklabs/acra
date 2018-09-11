21bdd7a2a1f18d390095ce6dd315524bee33f8d4
Lagovas <lagovas.lagovas@gmail.com>
2018-09-11 12:18:15 +0300

    Rotate keys in database [T671] (#256)
    * add rotation of keys/data in database
    * move tests of EncodeToOctal to correct package

--- utils/utils.go
+++ utils/utils.go
@@ -19,11 +19,10 @@ package utils
 
 import (
 	"encoding/binary"
+	"fmt"
 	"github.com/cossacklabs/themis/gothemis/keys"
 	"io"
 	"io/ioutil"
-
-	"fmt"
 
 	log "github.com/sirupsen/logrus"
 	"os"
@@ -32,12 +31,12 @@ import (
 )
 
 const (
-	// SESSION_DATA_LIMIT maximum block size
-	SESSION_DATA_LIMIT = 8 * 1024 // 8 kb
+	// SessionDataLimit maximum block size
+	SessionDataLimit = 8 * 1024 // 8 kb
 )
 
 // ErrBigDataBlockSize represents data encoding error
-var ErrBigDataBlockSize = fmt.Errorf("Block size greater than %v", SESSION_DATA_LIMIT)
+var ErrBigDataBlockSize = fmt.Errorf("Block size greater than %v", SessionDataLimit)
 
 // WriteFull writes data to io.Writer.
 // if wr.Write will return n <= len(data) will
