c14b579938269181845f3343702fba4466e2fd6f
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2018-12-05 12:23:08 +0200

    add data encoders

---
+++ encryptor/dbDataCoder.go
@@ -0,0 +1,117 @@
+/*
+Copyright 2018, Cossack Labs Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package encryptor
+
+import (
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"github.com/cossacklabs/acra/sqlparser"
+	"github.com/sirupsen/logrus"
+)
+
+var pgHexStringPrefix = []byte{'\\', 'x'}
+
+type DBDataCoder interface {
+	Decode(sqlparser.Expr) ([]byte, error)
+	Encode(sqlparser.Expr, []byte) ([]byte, error)
+}
+
+var errUnsupportedExpression = errors.New("unsupported expression")
+
+type MysqlDBDataCoder struct{}
+
+func (*MysqlDBDataCoder) Decode(expr sqlparser.Expr) ([]byte, error) {
+	switch val := expr.(type) {
+	case *sqlparser.SQLVal:
+		switch val.Type {
+		case sqlparser.StrVal:
+			return val.Val, nil
+		case sqlparser.HexVal:
+			binValue := make([]byte, hex.DecodedLen(len(val.Val)))
+			_, err := hex.Decode(binValue, val.Val)
+			if err != nil {
+				logrus.WithError(err).Errorln("Can't decode hex string literal")
+				return nil, err
+			}
+			return binValue, nil
+		}
+	}
+	return nil, errUnsupportedExpression
+}
+func (*MysqlDBDataCoder) Encode(expr sqlparser.Expr, data []byte) ([]byte, error) {
+	switch val := expr.(type) {
+	case *sqlparser.SQLVal:
+		switch val.Type {
+		case sqlparser.StrVal:
+			return data, nil
+		case sqlparser.HexVal:
+			output := make([]byte, hex.EncodedLen(len(data)))
+			hex.Encode(output, data)
+			return output, nil
+		}
+	}
+	return nil, errUnsupportedExpression
+}
+
+type PostgresqlDBDataCoder struct{}
+
+func (*PostgresqlDBDataCoder) Decode(expr sqlparser.Expr) ([]byte, error) {
+	switch val := expr.(type) {
+	case *sqlparser.SQLVal:
+		switch val.Type {
+		case sqlparser.HexVal:
+			binValue := make([]byte, hex.DecodedLen(len(val.Val)))
+			_, err := hex.Decode(binValue, val.Val)
+			if err != nil {
+				logrus.WithError(err).Errorln("Can't decode hex string literal")
+				return nil, err
+			}
+			return binValue, err
+		case sqlparser.PgEscapeString, sqlparser.StrVal:
+			// 4 == "\x" + min 2 symbols for hex decoding
+			if len(val.Val) >= 4 && bytes.Equal(val.Val[:len(pgHexStringPrefix)], pgHexStringPrefix) {
+				hexValue := val.Val[len(pgHexStringPrefix):]
+				binValue := make([]byte, hex.DecodedLen(len(hexValue)))
+				_, err := hex.Decode(binValue, hexValue)
+				if err != nil {
+					return nil, err
+				}
+				return binValue, err
+			}
+		}
+	}
+	return nil, errUnsupportedExpression
+}
+
+func (*PostgresqlDBDataCoder) Encode(expr sqlparser.Expr, data []byte) ([]byte, error) {
+	switch val := expr.(type) {
+	case *sqlparser.SQLVal:
+		switch val.Type {
+		case sqlparser.HexVal:
+			output := make([]byte, hex.EncodedLen(len(data)))
+			hex.Encode(output, data)
+			return output, nil
+		case sqlparser.PgEscapeString, sqlparser.StrVal:
+			newVal := make([]byte, len(pgHexStringPrefix)+hex.EncodedLen(len(data)))
+			copy(newVal, pgHexStringPrefix)
+			hex.Encode(newVal[len(pgHexStringPrefix):], data)
+			return newVal, nil
+		}
+	}
+	return nil, errUnsupportedExpression
+}
