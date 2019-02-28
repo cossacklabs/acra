0a845ee3342fb28ee31699cc23daa1777e8a0430
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2019-02-28 18:11:44 +0200

    remove hardcoded test value store diffs as artifact

--- .circleci/check_configs.sh
+++ .circleci/check_configs.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
 
-#TEST_OUTPUT_FILE="${HOME}/tests_output/config_diffs.txt"
-rm "/tmp/confs/out.txt"
-TEST_OUTPUT_FILE="/tmp/confs/out.txt"
-
 function compare_configs {
     folder_a=$1
     folder_b=$2
     binaries=(server connector translator addzone webconfig rollback keymaker poisonrecordmaker authmanager rotate)
     for cmd in "${binaries[@]}"; do
-     cmp ${folder_a}/acra-${cmd}.yaml ${folder_b}/acra-${cmd}.yaml 1>>${TEST_OUTPUT_FILE}
+     cmp ${folder_a}/acra-${cmd}.yaml ${folder_b}/acra-${cmd}.yaml
      cmp_status="$?"
      if [[ "${cmp_status}" != "0" ]]; then
         status=1
      fi
     done
-    cat ${TEST_OUTPUT_FILE}
 }
 
 temp_configs=`mktemp -d`
-echo "generate fresh configs to temporary folder ${temp_configs}"
-bash configs/regenerate.sh ${temp_configs}
+bash configs/regenerate.sh ${temp_configs} 2>/dev/null
 
 status=0
 compare_configs configs ${temp_configs}
-echo "remove temporary folder ${temp_configs}"
+
 rm -rf ${temp_configs}
 exit ${status}
