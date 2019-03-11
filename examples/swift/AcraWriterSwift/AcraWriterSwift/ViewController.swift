afd4ec8ddd1141dcbce3d6f58ef8cb378b24c007
vixentael <vixentael@users.noreply.github.com>
2019-03-11 12:28:35 +0200

    Add Swift Acrawriter examples (#326)
    * clean up podspec from unused imports
    
    * add swift example; update examples to use new acrawriter (temp)
    
    * stick with themis 0.10.4 and above
    
    * link podspec to master
    
    * rm local podspec
    
    * use header search path
    
    * re-link to the newest master
    
    * re-link both examples to use latest acrawriter; add swift readme

---
+++ examples/swift/AcraWriterSwift/AcraWriterSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AcraWriterSwift
+//
+//  Created by Anastasiia on 3/5/19.
+//  Copyright © 2019 Cossack Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
