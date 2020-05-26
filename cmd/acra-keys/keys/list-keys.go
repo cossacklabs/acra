73bf636a0c38f66aa8d71470ac5b9b696d015890
Alexei Lozovsky <alexei@cossacklabs.com>
2020-05-26 16:54:33 +0300

    Printing keys as table
    The default output mode for "acra-keys list" is a human-readable table
    with key descriptions and IDs. It's nothing fancy but at least it looks
    somewhat readable and usable as a general overview.

--- cmd/acra-keys/keys/list-keys.go
+++ cmd/acra-keys/keys/list-keys.go
@@ -17,12 +17,54 @@
 package keys
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/cossacklabs/acra/keystore"
+	"github.com/cossacklabs/acra/utils"
 )
 
 // PrintKeys prints key list prettily into the given writer.
 func PrintKeys(keys []keystore.KeyDescription, writer io.Writer, params *CommandLineParams) error {
-	panic("not implemented")
+	return printKeysTable(keys, writer)
+}
+
+const (
+	purposeHeader = "Key purpose"
+	extraIDHeader = "Client/Zone ID"
+	idHeader      = "Key ID"
+)
+
+func printKeysTable(keys []keystore.KeyDescription, writer io.Writer) error {
+	maxPurposeLen := len(purposeHeader)
+	maxExtraIDLen := len(extraIDHeader)
+	for _, key := range keys {
+		if len(key.Purpose) > maxPurposeLen {
+			maxPurposeLen = len(key.Purpose)
+		}
+		if len(key.ClientID) > maxExtraIDLen {
+			maxExtraIDLen = len(key.ClientID)
+		}
+		if len(key.ZoneID) > maxExtraIDLen {
+			maxExtraIDLen = len(key.ZoneID)
+		}
+	}
+
+	fmt.Fprintf(writer, "%-*s  %-*s  %s\n", maxPurposeLen, purposeHeader, maxExtraIDLen, extraIDHeader, idHeader)
+
+	separator := make([]byte, maxPurposeLen+maxExtraIDLen+len(idHeader)+4)
+	utils.FillSlice(byte('-'), separator)
+	fmt.Fprintln(writer, string(separator))
+
+	for _, key := range keys {
+		var extraID string
+		if key.ClientID != nil {
+			extraID = string(key.ClientID)
+		}
+		if key.ZoneID != nil {
+			extraID = string(key.ZoneID)
+		}
+		fmt.Fprintf(writer, "%*s  %*s  %s\n", maxPurposeLen, key.Purpose, maxExtraIDLen, extraID, key.ID)
+	}
+	return nil
 }
