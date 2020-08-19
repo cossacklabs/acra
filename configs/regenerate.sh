32507e4a53f572fedb9f67312e4d4e851763a96f
Alexei Lozovsky <alexei@cossacklabs.com>
2020-08-19 21:07:52 +0300

    Improve config regeneration
    Update the script to regenerate configuration files for all binaries,
    now you don't need to add new binaries here for them to be hooked up
    for testing (tests already list all tools in the "cmd" directory).
    
    Additionally, check for preexisting binaries in the current directory
    which can be used to generate configuration files. Tests run this script
    multiple times and they already have all the binaries ready, there is no
    need to compile them with "go run" every time, shaving off a couple of
    seconds from the build time.

--- configs/regenerate.sh
+++ configs/regenerate.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-
-binaries=(server connector translator addzone webconfig rollback keymaker poisonrecordmaker authmanager rotate)
+#
+# Regenerate example configuration files.
+#
+# Run as
+#
+#     ./configs/regenerate.sh [dst-dir]
+#
+# from repository root. If dst-dir is not specified then configs are written
+# into the default directory (./configs).
 
 args="--dump_config"
+extra=
 
-
-for cmd in "${binaries[@]}"; do
- if [[ "$#" == "1" ]]; then
-    args="--dump_config --config_file=$1/acra-${cmd}.yaml"
- fi
- go run ./cmd/acra-${cmd}/*.go `echo ${args}`
+for cmd in $(ls ./cmd/ | grep ^acra-); do
+    if [[ $# == 1 ]]; then
+        extra="--config_file=$1/${cmd}.yaml"
+    fi
+    if [[ -f ./$cmd ]]; then
+        ./$cmd $args $extra
+    else
+        go run ./cmd/$cmd $args $extra
+    fi
 done
