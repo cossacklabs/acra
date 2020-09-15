7f2175c2180e436714eaa9e3a2e93eedba8747a8
Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>
2020-09-16 01:01:07 +0300

    Allow only 3 ineffassign issues
    Those are currently intentional and "under control"

--- .circleci/check_ineffassign.sh
+++ .circleci/check_ineffassign.sh
@@ -4,7 +4,7 @@
 # duplicate output to stderr so we can see it in console while `wc` is counting the lines
 result=$($GOPATH/bin/ineffassign . | grep -v "\.pb\.go\|\.y" | tee /dev/stderr | wc -l)
 
-if [[ $result -gt 10 ]]; then
+if [[ $result -gt 3 ]]; then
   echo "Too many ineffassign issues: $result"
   exit 1;
 else
