04cc243b247c261f7e89d1656ffff6633d982fc6
Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>
2020-09-29 14:24:37 +0300

    Improve database connection check scripts

--- .circleci/check_mysql_connection.sh
+++ .circleci/check_mysql_connection.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
-[ -n "$MYSQL_USER" ] || MYSQL_USER=test
-[ -n "$MYSQL_PASSWORD" ] || MYSQL_PASSWORD=test
+MYSQL_HOST="${MYSQL_HOST:-127.0.0.1}"
+MYSQL_USER="${MYSQL_USER:-test}"
+MYSQL_PASSWORD="${MYSQL_PASSWORD:-test}"
 
-[ -n "$NUM_PINGS" ] || NUM_PINGS=15
-[ -n "$DELAY" ] || DELAY=1
+NUM_PINGS="${NUM_PINGS:-15}"
+DELAY="${DELAY:-1}"
 
 for try in $(seq $NUM_PINGS); do
-    mysqladmin ping -h127.0.0.1 -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --protocol=TCP && exit 0 || sleep $DELAY
+    mysqladmin ping \
+        -h"$MYSQL_HOST" \
+        -u"$MYSQL_USER" \
+        -p"$MYSQL_PASSWORD" \
+        --protocol=TCP \
+        && exit 0 || sleep $DELAY
 done
 
-echo "Cannot reach the database"
+echo "Cannot reach the database at $MYSQL_HOST"
 exit 1
--- .circleci/check_postgresql_connection.sh
+++ .circleci/check_postgresql_connection.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+set -e
 
-[ -n "$POSTGRES_USER" ] || POSTGRES_USER=test
-[ -n "$POSTGRES_DB" ] || POSTGRES_DB=test
+POSTGRES_HOST="${POSTGRES_HOST:-127.0.0.1}"
+POSTGRES_USER="${POSTGRES_USER:-test}"
+POSTGRES_DB="${POSTGRES_DB:-test}"
 
-[ -n "$NUM_PINGS" ] || NUM_PINGS=15
-[ -n "$DELAY" ] || DELAY=1
+NUM_PINGS="${NUM_PINGS:-15}"
+DELAY="${DELAY:-1}"
 
 for try in $(seq $NUM_PINGS); do
-    pg_isready -U"${POSTGRES_USER}" -d"${POSTGRES_DB}" -h127.0.0.1 && exit 0 || sleep $DELAY
+    pg_isready \
+        -h"$POSTGRES_HOST" \
+        -U"$POSTGRES_USER" \
+        -d"$POSTGRES_DB" \
+        && exit 0 || sleep $DELAY
 done
 
-echo "Cannot reach the database"
+echo "Cannot reach the database at $POSTGRES_HOST"
 exit 1
