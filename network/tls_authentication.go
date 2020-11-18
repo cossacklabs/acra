977740cc2d7a98f44fdfe39fd7e7cfdfde445f77
Lagovas <lagovas.lagovas@gmail.com>
2020-11-18 21:43:41 +0200

    add identifier extractors from peer certificate which may be used as client_id

--- network/tls_authentication.go
+++ network/tls_authentication.go
@@ -1,9 +1,72 @@
 package network
 
 import (
+	"crypto/sha512"
 	"crypto/x509"
+	"encoding/hex"
 	"errors"
+	"github.com/cossacklabs/acra/keystore"
 )
+
+// CertificateIdentifierExtractor interface for implementations which should return identifier used for client's identification
+type CertificateIdentifierExtractor interface {
+	GetCertificateIdentifier(certificate *x509.Certificate) ([]byte, error)
+}
+
+// CommonNameExtractor implementation for CertificateIdentifierExtractor interface, which return CommonName as client's identifier
+type CommonNameExtractor struct{}
+
+// GetCertificateIdentifier return Subject.String() as client's identifier by tls certificate
+func (e CommonNameExtractor) GetCertificateIdentifier(certificate *x509.Certificate) ([]byte, error) {
+	if certificate == nil {
+		return nil, ErrNoPeerCertificate
+	}
+	return []byte(certificate.Subject.String()), nil
+}
+
+// SerialNumberExtractor implementation for CertificateIdentifierExtractor interface, which return SerialNumber of certificate as client's identifier
+type SerialNumberExtractor struct{}
+
+// GetCertificateIdentifier return SerialNumber as client's identifier by tls certificate
+func (e SerialNumberExtractor) GetCertificateIdentifier(certificate *x509.Certificate) ([]byte, error) {
+	if certificate == nil {
+		return nil, ErrNoPeerCertificate
+	}
+	return certificate.SerialNumber.Bytes(), nil
+}
+
+// ErrEmptyIdentifier used when passed empty identifier with zero length
+var ErrEmptyIdentifier = errors.New("empty identifier")
+
+// IdentifierConverter converts identifiers from x509 certificates to clientID format acceptable by keystore, pass keystore.ValidateID check
+type IdentifierConverter interface {
+	Convert(identifier []byte)([]byte, error)
+}
+
+// HexIdentifierConverter converts identifiers to hex value
+type HexIdentifierConverter struct {}
+var hexStaticPrefix = []byte{0}
+// Convert identifier to hex value. If len(identifier) == 1 then 0 inserted as start of identifier to match minimal length
+// of clientID 4 bytes. If len(identifier) > (keystore.MaxClientIDLength / 2) than it longer than max acceptable length of clientID in hex format (256)
+// In such case identifier passed through SHA512 and then converted to hex with 128 (64 * 2) bytes length
+func (c HexIdentifierConverter) Convert(identifier[]byte)([]byte, error){
+	var out []byte
+	if len(identifier) == 1 {
+		out = make([]byte, hex.EncodedLen(len(identifier) + len(hexStaticPrefix)))
+		identifier = append(hexStaticPrefix, identifier...)
+	} else if len(identifier) > (keystore.MaxClientIDLength / 2) {
+		out = make([]byte, hex.EncodedLen(sha512.Size))
+		h := sha512.New()
+		if _, err := h.Write(identifier); err != nil {
+			return nil, err
+		}
+		identifier = h.Sum(nil)
+	} else {
+		out = make([]byte, hex.EncodedLen(len(identifier)))
+	}
+	hex.Encode(out, identifier)
+	return out, nil
+}
 
 // Set of errors related to peer certificate validation
 var (
--- network/tls_authentication_test.go
+++ network/tls_authentication_test.go
@@ -1,9 +1,75 @@
 package network
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha512"
 	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+// getWorkingDirectory expects that tests started from root of source code and accessible tests/ssl folder, otherwise call t.Fatal otherwise
+func getWorkingDirectory(t *testing.T) string {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sslTestFolder := filepath.Join(workingDirectory, "tests/ssl")
+	info, err := os.Lstat(sslTestFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !info.IsDir() {
+		t.Fatalf("'%s' is not directory as expected\n", sslTestFolder)
+	}
+	return workingDirectory
+}
+
+func getAcraWriterTestx509Certificate(t *testing.T) *x509.Certificate {
+	certPath := filepath.Join(getWorkingDirectory(t), "tests/ssl/acra-writer/acra-writer.crt")
+	certificateData, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	block, _ := pem.Decode(certificateData)
+	if block == nil {
+		t.Fatal("Empty block from PEM certificate")
+	}
+
+	certificate, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return certificate
+}
+
+func TestSerialNumberExtractor_GetCertificateIdentifier(t *testing.T) {
+	certificate := getAcraWriterTestx509Certificate(t)
+	serialNumber := certificate.SerialNumber.Bytes()
+	identifier, err := SerialNumberExtractor{}.GetCertificateIdentifier(certificate)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(serialNumber, identifier) {
+		t.Fatal("SerialNumberExtractor return something else than serial number as identifier")
+	}
+}
+func TestCommonNameExtractor_GetCertificateIdentifier(t *testing.T) {
+	certificate := getAcraWriterTestx509Certificate(t)
+	commonName := []byte(certificate.Subject.String())
+	identifier, err := CommonNameExtractor{}.GetCertificateIdentifier(certificate)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(commonName, identifier) {
+		t.Fatal("CommonNameExtractor return something else than Subject as DN as identifier")
+	}
+}
 
 func TestValidateClientsAuthenticationCertificate(t *testing.T) {
 	if err := ValidateClientsAuthenticationCertificate(nil); err != ErrNoPeerCertificate {
@@ -45,7 +111,7 @@ func TestValidateClientsAuthenticationCertificate(t *testing.T) {
 
 	// test only with correct KeyUsage
 	certificate = &x509.Certificate{
-		KeyUsage: invalidKeyUsage | x509.KeyUsageDigitalSignature,
+		KeyUsage:    invalidKeyUsage | x509.KeyUsageDigitalSignature,
 		ExtKeyUsage: invalidExtKeyUsage}
 	if err := ValidateClientsAuthenticationCertificate(certificate); err != nil {
 		t.Fatalf("Took %s error instead nil\n", err)
@@ -53,7 +119,7 @@ func TestValidateClientsAuthenticationCertificate(t *testing.T) {
 
 	// test only with correct ExtKeyUsage
 	certificate = &x509.Certificate{
-		KeyUsage: invalidKeyUsage,
+		KeyUsage:    invalidKeyUsage,
 		ExtKeyUsage: append(invalidExtKeyUsage, x509.ExtKeyUsageClientAuth)}
 	if err := ValidateClientsAuthenticationCertificate(certificate); err != nil {
 		t.Fatalf("Took %s error instead nil\n", err)
@@ -61,10 +127,51 @@ func TestValidateClientsAuthenticationCertificate(t *testing.T) {
 
 	// test only with correct ExtKeyUsage
 	certificate = &x509.Certificate{
-		KeyUsage: invalidKeyUsage | x509.KeyUsageDigitalSignature,
+		KeyUsage:    invalidKeyUsage | x509.KeyUsageDigitalSignature,
 		ExtKeyUsage: append(invalidExtKeyUsage, x509.ExtKeyUsageClientAuth)}
 	if err := ValidateClientsAuthenticationCertificate(certificate); err != nil {
 		t.Fatalf("Took %s error instead nil\n", err)
 	}
+}
 
+func TestHexIdentifierConverter_ConvertLength1(t *testing.T) {
+	identifier := []byte{1}
+	result, err := HexIdentifierConverter{}.Convert(identifier)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedResult := []byte(hex.EncodeToString(append(hexStaticPrefix, identifier...)))
+	if !bytes.Equal(result, expectedResult) {
+		t.Fatal("Incorrect converted value for identifier with length==1")
+	}
+}
+func TestHexIdentifierConverter_ConvertLength128(t *testing.T) {
+	identifier := make([]byte, 128)
+	if _, err := rand.Read(identifier); err != nil {
+		t.Fatal(err)
+	}
+	result, err := HexIdentifierConverter{}.Convert(identifier)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedResult := []byte(hex.EncodeToString(identifier))
+	if !bytes.Equal(result, expectedResult) {
+		t.Fatal("Incorrect converted value for identifier with length == 128, when should be the same value in hex format")
+	}
+}
+func TestHexIdentifierConverter_ConvertLengthLongerThan128(t *testing.T) {
+	identifier := make([]byte, 129)
+	if _, err := rand.Read(identifier); err != nil {
+		t.Fatal(err)
+	}
+	result, err := HexIdentifierConverter{}.Convert(identifier)
+	if err != nil {
+		t.Fatal(err)
+	}
+	h := sha512.New()
+	h.Write(identifier)
+	expectedResult := []byte(hex.EncodeToString(h.Sum(nil)))
+	if !bytes.Equal(result, expectedResult) {
+		t.Fatal("Incorrect converted value for identifier with length > 128, when should be the hex value after sha512")
+	}
 }
