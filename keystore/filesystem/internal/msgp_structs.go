1cbf02a629b23fc673b838d8e337d697d815cf5b
Lagovas <lagovas.lagovas@gmail.com>
2022-02-16 23:27:29 +0200

    Update keystore/filesystem/internal/msgp_structs.go
    Co-authored-by: Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>

--- keystore/filesystem/internal/msgp_structs.go
+++ keystore/filesystem/internal/msgp_structs.go
@@ -1,4 +1,4 @@
-// Copyright 2016, Cossack Labs Limited
+// Copyright 2022, Cossack Labs Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
