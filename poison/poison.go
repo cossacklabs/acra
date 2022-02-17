d0754eebd37223be534d5b7a0b8d83f0d12a2efd
Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>
2022-02-17 16:38:57 +0200

    Remove unused code

--- poison/poison.go
+++ poison/poison.go
@@ -22,7 +22,6 @@ limitations under the License.
 package poison
 
 import (
-	"context"
 	"crypto/rand"
 	math_rand "math/rand"
 	"time"
@@ -30,10 +29,7 @@ import (
 	"github.com/cossacklabs/acra/acrablock"
 	"github.com/cossacklabs/acra/acrastruct"
 	"github.com/cossacklabs/acra/crypto"
-	"github.com/cossacklabs/acra/decryptor/base"
 	"github.com/cossacklabs/acra/keystore"
-	"github.com/cossacklabs/acra/logging"
-	"github.com/cossacklabs/acra/utils"
 	"github.com/cossacklabs/themis/gothemis/keys"
 )
 
@@ -103,103 +99,4 @@ func CreateSymmetricPoisonRecord(keyStore keystore.PoisonKeyStore, dataLength in
 type RecordProcessorKeyStore interface {
 	GetPoisonPrivateKeys() ([]*keys.PrivateKey, error)
 	GetPoisonSymmetricKeys() ([][]byte, error)
-}
-
-// RecordProcessor implements DecryptionSubscriber interface to subscribe on AcraStructs and detect poison records
-type RecordProcessor struct {
-	keystore       RecordProcessorKeyStore
-	callbacks      base.PoisonRecordCallbackStorage
-	poisonDetector *crypto.EnvelopeDetector
-}
-
-//// NewRecordProcessor return new RecordProcessor
-//func NewRecordProcessor(keystore RecordProcessorKeyStore, callbacks base.PoisonRecordCallbackStorage) (*RecordProcessor, error) {
-//	registryHandler := crypto.NewRegistryHandler(crypto.NewPoisonRecordKeyStoreWrapper(keystore))
-//	envelopeDetector := crypto.NewEnvelopeDetector()
-//	if callbacks != nil && callbacks.HasCallbacks() {
-//		// setting PoisonRecords callback for CryptoHandlers inside registry
-//		poisonDetector := crypto.NewPoisonRecordsRecognizer(keystore, registryHandler)
-//		poisonDetector.SetPoisonRecordCallbacks(callbacks)
-//		envelopeDetector.AddCallback(poisonDetector)
-//	}
-//	return &RecordProcessor{keystore, callbacks, envelopeDetector}, nil
-//}
-
-// ID return string id of processor
-func (processor *RecordProcessor) ID() string {
-	return "RecordProcessor"
-}
-
-// OnColumn try to detect any poison record (inlined too) in data and call callbacks from PoisonRecordCallbackStorage
-func (processor *RecordProcessor) OnColumn(ctx context.Context, data []byte) (context.Context, []byte, error) {
-	logger := logging.GetLoggerFromContext(ctx)
-	logger.Debugln("Called OnColumn in poison processor")
-	// safe to pass same buffer because processor will return same data
-	if _, err := acrastruct.ProcessAcraStructs(ctx, data, data, processor); err != nil {
-		return ctx, data, err
-	}
-	_, err := acrablock.ProcessAcraBlocks(ctx, data, data, processor)
-	return ctx, data, err
-}
-
-// OnAcraStruct callback for every recognized AcraStruct which try to decrypt with poison record private keys
-func (processor *RecordProcessor) OnAcraStruct(ctx context.Context, acrastructBlock []byte) ([]byte, error) {
-	logger := logging.GetLoggerFromContext(ctx)
-	if !processor.callbacks.HasCallbacks() {
-		logger.Debugln("Skip poison record check due to empty callbacks")
-		return acrastructBlock, nil
-	}
-	logger.Debugln("Called on AcraStruct in poison processor")
-	poisonKeys, err := processor.keystore.GetPoisonPrivateKeys()
-	if err != nil {
-		logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorCantReadKeys).WithError(err).Errorln("Can't load poison keypair")
-		return acrastructBlock, err
-	}
-	defer utils.ZeroizePrivateKeys(poisonKeys)
-
-	_, err = acrastruct.DecryptRotatedAcrastruct(acrastructBlock, poisonKeys, nil)
-	if err == nil {
-		logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorDecryptorRecognizedPoisonRecord).Warningln("Recognized poison record")
-		if processor.callbacks.HasCallbacks() {
-			err = processor.callbacks.Call()
-			if err != nil {
-				logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorDecryptorCantCheckPoisonRecord).WithError(err).Errorln("Unexpected error in poison record callbacks")
-			}
-			logger.Debugln("Processed all callbacks on poison record")
-			return acrastructBlock, err
-		}
-		return acrastructBlock, nil
-	}
-	return acrastructBlock, nil
-}
-
-// OnAcraBlock callback for every recognized AcraBlock which try to decrypt with poison record symmetric keys
-func (processor *RecordProcessor) OnAcraBlock(ctx context.Context, acraBlock acrablock.AcraBlock) ([]byte, error) {
-	logger := logging.GetLoggerFromContext(ctx)
-	if !processor.callbacks.HasCallbacks() {
-		logger.Debugln("Skip poison record check due to empty callbacks")
-		return acraBlock, nil
-	}
-	logger.Debugln("Called on AcraBlock in poison processor")
-	poisonKeys, err := processor.keystore.GetPoisonSymmetricKeys()
-	if err != nil {
-		logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorCantReadKeys).WithError(err).Errorln("Can't load poison symmetric keys")
-		return acraBlock, err
-	}
-	defer utils.ZeroizeSymmetricKeys(poisonKeys)
-
-	_, err = acraBlock.Decrypt(poisonKeys, nil)
-	if err == nil {
-		logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorDecryptorRecognizedPoisonRecord).Warningln("Recognized poison record")
-		if processor.callbacks.HasCallbacks() {
-			err = processor.callbacks.Call()
-			if err != nil {
-				logger.WithField(logging.FieldKeyEventCode, logging.EventCodeErrorDecryptorCantCheckPoisonRecord).WithError(err).Errorln("Unexpected error in poison record callbacks")
-			}
-			logger.Debugln("Processed all callbacks on poison record")
-			return acraBlock, err
-		}
-		return acraBlock, nil
-	}
-	return acraBlock, nil
 }
