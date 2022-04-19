baf75775f83b23058a1e804d1fe45c9c94218b24
Nazar Serhiichuk <43041209+G1gg1L3s@users.noreply.github.com>
2022-04-19 18:16:13 +0300

    Fix `acra-keys list` with keystore v1 (#530)
    * Fix double listing of keys in the same directory
    
    The v1 keystore can have two paths for public and private keys.
    But most often they are the same. The `ListKeys` function of keystore
    doesn't check if these paths are the same, and if yes, enumerates all
    keys twice.
    
    Change this by checking the paths.
    
    * Fix printing of log keys
    
    The filename is `secure_log_key` and it's parsed as a regular client
    key, which consists of `<client_id>_<purpose>_<kind>`. So in case of
    `secure_log_key` the client_id is `secure, which is not correct.
    
    Fix this by setting empty array as a ClientId.
    
    * Update changelog
    
    [skip ci]
    
    * Move table printing code into utils
    
    Removes code duplication.

---
+++ keystore/utils.go
@@ -0,0 +1,57 @@
+package keystore
+
+import (
+	"fmt"
+	"io"
+)
+
+// PrintKeysTable prints table which describes keys in a human readable format
+// into the writer.
+// Code is shared by `acra-keys list` and a couple of tests
+func PrintKeysTable(keys []KeyDescription, writer io.Writer) error {
+	const (
+		purposeHeader = "Key purpose"
+		extraIDHeader = "Client/Zone ID"
+		idHeader      = "Key ID"
+	)
+
+	maxPurposeLen := len(purposeHeader)
+	maxExtraIDLen := len(extraIDHeader)
+	maxKeyIDLen := len(idHeader)
+	for _, key := range keys {
+		if len(key.Purpose) > maxPurposeLen {
+			maxPurposeLen = len(key.Purpose)
+		}
+		if len(key.ClientID) > maxExtraIDLen {
+			maxExtraIDLen = len(key.ClientID)
+		}
+		if len(key.ZoneID) > maxExtraIDLen {
+			maxExtraIDLen = len(key.ZoneID)
+		}
+		if len(key.ID) > maxKeyIDLen {
+			maxKeyIDLen = len(key.ID)
+		}
+	}
+
+	fmt.Fprintf(writer, "%-*s | %-*s | %s\n", maxPurposeLen, purposeHeader, maxExtraIDLen, extraIDHeader, idHeader)
+
+	separator := make([]byte, maxPurposeLen+maxExtraIDLen+maxKeyIDLen+6)
+	for i := range separator {
+		separator[i] = '-'
+	}
+	separator[maxPurposeLen+1] = byte('+')
+	separator[maxPurposeLen+maxExtraIDLen+4] = byte('+')
+	fmt.Fprintln(writer, string(separator))
+
+	for _, key := range keys {
+		var extraID string
+		if key.ClientID != nil {
+			extraID = string(key.ClientID)
+		}
+		if key.ZoneID != nil {
+			extraID = string(key.ZoneID)
+		}
+		fmt.Fprintf(writer, "%-*s | %-*s | %s\n", maxPurposeLen, key.Purpose, maxExtraIDLen, extraID, key.ID)
+	}
+	return nil
+}
