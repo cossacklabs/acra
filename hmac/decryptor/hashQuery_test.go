d0adee4c69093706f77c7b8a748302b4fcd3545e
Artem Zhmaka <zhmakas99@gmail.com>
2022-10-04 08:50:17 +0100

    zhars/extend_syntax_for_searchable_encryption
    Added unit test for searchable encryption extended syntax

--- hmac/decryptor/hashQuery_test.go
+++ hmac/decryptor/hashQuery_test.go
@@ -3,6 +3,7 @@ package decryptor
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"github.com/cossacklabs/acra/crypto"
 	"github.com/cossacklabs/acra/decryptor/base"
 	"github.com/cossacklabs/acra/decryptor/base/mocks"
@@ -10,6 +11,7 @@ import (
 	"github.com/cossacklabs/acra/encryptor/config"
 	mocks2 "github.com/cossacklabs/acra/keystore/mocks"
 	"github.com/cossacklabs/acra/sqlparser"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 )
@@ -106,5 +108,99 @@ func TestSearchablePreparedStatementsWithTextFormat(t *testing.T) {
 		if bytes.Equal(newData, sourceBindValue) {
 			t.Fatal("Data wasn't changed")
 		}
+	}
+}
+
+// TestSearchableWithTextFormat process searchable SELECT query without placeholders with text format
+func TestSearchableWithTextFormat(t *testing.T) {
+	clientSession := &mocks.ClientSession{}
+	sessionData := make(map[string]interface{}, 2)
+	clientSession.On("GetData", mock.Anything).Return(func(key string) interface{} {
+		return sessionData[key]
+	}, func(key string) bool {
+		_, ok := sessionData[key]
+		return ok
+	})
+	clientSession.On("DeleteData", mock.Anything).Run(func(args mock.Arguments) {
+		delete(sessionData, args[0].(string))
+	})
+	clientSession.On("SetData", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		sessionData[args[0].(string)] = args[1]
+	})
+	schemaConfig := `schemas:
+  - table: test_table
+    columns:
+      - data1
+      - data2
+    encrypted:
+      - column: data1
+        searchable: true`
+
+	schema, err := config.MapTableSchemaStoreFromConfig([]byte(schemaConfig))
+	assert.NoError(t, err)
+
+	ctx := base.SetClientSessionToContext(context.Background(), clientSession)
+	parser := sqlparser.New(sqlparser.ModeDefault)
+	keyStore := &mocks2.ServerKeyStore{}
+	keyStore.On("GetHMACSecretKey", mock.Anything).Return([]byte(`some key`), nil)
+	registryHandler := crypto.NewRegistryHandler(nil)
+	encryptor := NewPostgresqlHashQuery(keyStore, schema, registryHandler)
+	dataQueryPart := "test-data"
+
+	coder := &encryptor2.PostgresqlDBDataCoder{}
+
+	type testcase struct {
+		Query string
+	}
+	testcases := []testcase{
+		{Query: "SELECT data1 from test_table WHERE data1='%s'"},
+		{Query: "UPDATE test_table SET kind = 'kind' WHERE data1='%s'"},
+		{Query: "INSERT INTO table2 SELECT * FROM test_table WHERE data1='%s' and data2='other-data'"},
+		{Query: "DELETE FROM test_table WHERE data1='%s'"},
+		{Query: "DELETE FROM test_table WHERE data1='%s' OR data2='other-data'"},
+	}
+	for _, testcase := range testcases {
+		query := fmt.Sprintf(testcase.Query, dataQueryPart)
+
+		queryObj := base.NewOnQueryObjectFromQuery(query, parser)
+		queryObj, _, err = encryptor.OnQuery(ctx, queryObj)
+		assert.NoError(t, err)
+
+		stmt, err := queryObj.Statement()
+		assert.NoError(t, err)
+
+		var whereStatements []*sqlparser.Where
+		err = sqlparser.Walk(func(node sqlparser.SQLNode) (kontinue bool, err error) {
+			where, ok := node.(*sqlparser.Where)
+			if ok {
+				whereStatements = append(whereStatements, where)
+			}
+			return true, nil
+		}, stmt)
+		assert.NoError(t, err)
+		assert.True(t, len(whereStatements) > 0)
+
+		var comparisonExpr *sqlparser.ComparisonExpr
+		switch node := whereStatements[0].Expr.(type) {
+		case *sqlparser.ComparisonExpr:
+			comparisonExpr = node
+		case *sqlparser.AndExpr:
+			comparisonExpr = node.Left.(*sqlparser.ComparisonExpr)
+		case *sqlparser.OrExpr:
+			comparisonExpr = node.Left.(*sqlparser.ComparisonExpr)
+		}
+
+		_, isSubstrExpr := comparisonExpr.Left.(*sqlparser.SubstrExpr)
+		assert.True(t, isSubstrExpr)
+
+		rightVal := comparisonExpr.Right.(*sqlparser.SQLVal)
+		assert.NotEqual(t, dataQueryPart, string(rightVal.Val))
+
+		hmacValue, err := encryptor.calculateHmac(ctx, []byte(dataQueryPart))
+		assert.NoError(t, err)
+
+		newData, err := coder.Encode(rightVal, hmacValue)
+		assert.NoError(t, err)
+		assert.Equal(t, len(rightVal.Val), len(newData))
 	}
 }
